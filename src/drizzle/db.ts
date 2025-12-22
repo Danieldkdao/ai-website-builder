45507c6ae138eebc75edeec15f041c9bf0e220d8
Daniel Dao <danieldkdao@gmail.com>
2025-12-22 15:59:51 +0700

    02:database

---
+++ src/drizzle/db.ts
@@ -0,0 +1,6 @@
+import { envServer } from "@/data/env/server";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+const sql = neon(envServer.DATABASE_URL);
+export const db = drizzle({ client: sql });
