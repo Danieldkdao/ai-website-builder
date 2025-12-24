900d244c7453549b1bc0f057aa9c525b1feb7dfb
Daniel Dao <danieldkdao@gmail.com>
2025-12-24 22:55:54 +0700

    08:messages

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,2 @@
+export * from "./schemas/message";
+export * from "./schemas/fragment";
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,22 +4,30 @@ import {
   createAgent,
   createTool,
   createNetwork,
+  type Tool,
 } from "@inngest/agent-kit";
 import { Sandbox } from "@e2b/code-interpreter";
 import { getSandbox, lastAssistantTextMessageContent } from "./utils";
 import z from "zod";
 import { PROMPT } from "@/prompt";
+import { db } from "@/drizzle/db";
+import { FragmentTable, MessageTable } from "@/drizzle/schema";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+type AgentState = {
+  summary: string;
+  files: { [path: string]: string };
+};
+
+export const codeAgentFunction = inngest.createFunction(
+  { id: "code-agent" },
+  { event: "code-agent/run" },
   async ({ event, step }) => {
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("awb-nextjs-dd");
       return sandbox.sandboxId;
     });
 
-    const codeAgent = createAgent({
+    const codeAgent = createAgent<AgentState>({
       name: "code-agent",
       description: "An expert coding agent.",
       system: PROMPT,
@@ -67,7 +75,10 @@ export const helloWorld = inngest.createFunction(
               })
             ),
           }),
-          handler: async ({ files }, { step, network }) => {
+          handler: async (
+            { files },
+            { step, network }: Tool.Options<AgentState>
+          ) => {
             const newFiles = await step?.run(
               "createOrUpdateFiles",
               async () => {
@@ -129,7 +140,7 @@ export const helloWorld = inngest.createFunction(
       },
     });
 
-    const network = createNetwork({
+    const network = createNetwork<AgentState>({
       name: "coding-agent-network",
       agents: [codeAgent],
       maxIter: 15,
@@ -144,10 +155,42 @@ export const helloWorld = inngest.createFunction(
 
     const result = await network.run(event.data.value);
 
+    const isError =
+      !result.state.data.summary ||
+      Object.keys(result.state.data.files || {}).length === 0;
+
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       const sandbox = await getSandbox(sandboxId);
       const host = sandbox.getHost(3000);
       return `https://${host}`;
+    });
+
+    await step.run("save-result", async () => {
+      if (isError) {
+        return await db.insert(MessageTable).values({
+          content: "Something went wrong. Please try again.",
+          role: "assistant",
+          type: "error",
+        });
+      }
+      const [newMessage] = await db
+        .insert(MessageTable)
+        .values({
+          content: result.state.data.summary,
+          role: "assistant",
+          type: "result",
+        })
+        .returning();
+
+      if (newMessage) {
+        await db.insert(FragmentTable).values({
+          messageId: newMessage.id,
+          sandboxUrl,
+          title: "Fragment",
+          files: result.state.data.files,
+        });
+      }
+      return newMessage;
     });
 
     return {
